cc4054b8afcd0b6c7b2da6d5b6e0f34f017b2c7a
NDMHVLC <nedimhavlaci@gmail.com>
2022-08-23 19:02:23 +0300

    23.08

---
+++ src/demoProjects/rentACar/Application/Features/Commands/CreateBrand/CreateBrandCommand.cs
@@ -0,0 +1,46 @@
+﻿using Application.Features.Dtos;
+using Application.Features.Rules;
+using Application.Services.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Commands.CreateBrand
+{
+    public class CreateBrandCommand : IRequest<CreatedBrandDto>
+    {
+        public string Name { get; set; }
+
+        public class CreatedBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreatedBrandDto>
+        {
+            private readonly IBrandRepository _brandRepository;
+            private readonly IMapper _mapper;
+            private readonly BrandsBusinessRules _brandsBusinessRules;
+
+            public CreatedBrandCommandHandler(IBrandRepository brandRepository, IMapper mapper,
+                BrandsBusinessRules brandsBusinessRules)
+            {
+                _brandRepository = brandRepository;
+                _mapper = mapper;
+                _brandsBusinessRules = brandsBusinessRules;
+            }
+
+            public async Task<CreatedBrandDto> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+            {
+                await _brandsBusinessRules.BrandNameCanNotBeDuplicatedWhenInserted(request.Name);
+
+                Brand mapperBrand = _mapper.Map<Brand>(request);
+                Brand createdBrand = await _brandRepository.AddAsync(mapperBrand);
+                CreatedBrandDto createdBrandDto = _mapper.Map<CreatedBrandDto>(createdBrand);
+
+                return createdBrandDto;
+            }
+        }
+    }
+}
---
+++ src/demoProjects/rentACar/WebAPI/Controllers/BrandsController.cs
@@ -0,0 +1,20 @@
+﻿using Application.Features.Commands.CreateBrand;
+using Application.Features.Dtos;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandsController : BaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] CreateBrandCommand createBrandCommand)
+        {
+            CreatedBrandDto result = await Mediator.Send(createBrandCommand);
+            return Created("", result);
+        }
+    }
+}
